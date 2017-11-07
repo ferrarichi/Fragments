4fc770377e5c774e6b06ba85de8e414dcc5f6564
ferrarichi <juanjo29698@gmail.com>
2017-11-07 17:56:18 +0100

    Initial commit

---
+++ app/src/main/java/com/example/juanjo/fragments/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.juanjo.fragments;
+
+import android.content.SyncStatusObserver;
+import android.net.Uri;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+}
+
+
+
+}
